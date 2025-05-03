4b9d77a049b1e234562c073e9acf56b4b6fc79bd
AlanCoutinhoD <alanemmanueldiazcoutino@gmail.com>
2025-05-03 22:32:27 +0200

    registro de usuarios desactivado

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 // Ruta de registro
-router.post('/register', authController.register);
+//router.post('/register', authController.register);
 // Ruta de login
 router.post('/login', authController.login);
 
